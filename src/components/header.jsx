9fff24d25b248a5cf7fd85059f763b2133528a43
Sanskruti Nijai <nijaisanskruti@gmail.com>
2025-07-20 23:54:33 +0530

    protected routes, making header dynamic+logout logic

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -11,11 +11,18 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { LinkIcon, LogOut } from 'lucide-react';
+import { UrlState } from '@/context';
+import useFetch from '@/hooks/use-fetch';
+import { BarLoader } from 'react-spinners';
+import { logout } from '@/db/apiAuth';
 
 const Header = () => {
     const navigate = useNavigate();
-    const user = false;
+    const {user, fetchUser} = UrlState();
+
+    const {loading, fn: fnLogout} = useFetch(logout);
   return (
+    <>
     <nav className='py-4 flex justify-between items-center'>
         <Link to="/">
             <img src="/logo.jpg" className='h-16' alt='Trimrr logo' />
@@ -27,27 +34,33 @@ const Header = () => {
                     <DropdownMenu>
   <DropdownMenuTrigger className='w-10 rounded-full overflow-hidden'>
     <Avatar>
-  <AvatarImage src="https://github.com/shadcn.png" />
+  <AvatarImage src={user?.user_metadata?.profile_pic} className="object-contain"/>
   <AvatarFallback>PA</AvatarFallback>
 </Avatar>
   </DropdownMenuTrigger>
   <DropdownMenuContent>
-    <DropdownMenuLabel>Sanskruti Nijai</DropdownMenuLabel>
+    <DropdownMenuLabel>{user?.user_metadata?.name}</DropdownMenuLabel>
     <DropdownMenuSeparator />
     <DropdownMenuItem>
         <LinkIcon className='mr-2 h-4 w-4' />
         My Links
     </DropdownMenuItem>
     <DropdownMenuItem className='text-red-400'>
-        <LogOut className='mr-2 h-4 w-4' /> <span>Logout</span>
+        <LogOut className='mr-2 h-4 w-4' /> <span onClick={() => {
+          fnLogout().then(() => {
+            fetchUser();
+            navigate("/");
+          })
+        }}>Logout</span>
     </DropdownMenuItem>
     </DropdownMenuContent>
   </DropdownMenu>
                 )
             }
-            
         </div>
+        {loading && <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />}
     </nav>
+    </>
   );
 };
 
