a84760376dcaf04927e06240baed73c049395c3e
Sanskruti Nijai <nijaisanskruti@gmail.com>
2025-07-21 23:54:40 +0530

    dashboard creation

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { BarLoader } from 'react-spinners';
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div>
+      {true && <BarLoader width={"100%"} color="#36d7b7" />}
+      <div>
+  
+      </div>
+      <Card>
+         <CardHeader>
+          <CardTitle>Links Created</CardTitle>
+         </CardHeader>
+         <CardContent>
+          <p>0</p>
+         </CardContent>
+      </Card>
+      <Card>
+         <CardHeader>
+          <CardTitle>Total Clicks</CardTitle>
+         </CardHeader>
+         <CardContent>
+          <p>0</p>
+         </CardContent>
+      </Card>
+    </div>
   )
 }
 
